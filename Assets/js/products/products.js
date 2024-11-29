fdeef806f40450f1cb373d16c92d86866f3c7052
Jonh Alex <122692601+Jonhvmp@users.noreply.github.com>
2024-11-29 14:51:59 -0300

    Adicionar classe Products com métodos para gerenciar produtos no banco de dados
    Co-authored-by: Riane Ferreira Menezes <rianefm@users.noreply.github.com>

---
+++ Assets/js/products/products.js
@@ -0,0 +1,47 @@
+import { Database } from "../database";
+
+export class Products {
+  constructor() {
+    this.db = new Database();
+    this.storeName = this.db.storeNames.products;
+  }
+
+  async addProduct(name, price, description = "") {
+    if (!name || price === undefined || price < 0) {
+      throw new Error('Nome e preço são obrigatórios, e o preço deve ser maior ou igual a 0');
+    }
+    const product = {
+      name,
+      price,
+      description
+    }
+  return await this.db.addItem(this.storeName, product)
+  }
+
+  async getAllProducts(){
+    return await this.db.getAllItems(this.storeName)
+  }
+
+  async getProductById(id){
+   if(!id){
+    throw new Error('Id é obrigatório para buscar um produto.');
+   }
+   return await this.db.getItemByKey(this.storeName, id)
+  }
+
+}
+
+
+/**
+ * Atualiza um produto existente.
+ * @param {number} id - ID do produto a ser atualizado.
+ * @param {object} updates Dados a serem atualizados ({ name, price, description }).
+ * @returns {Promise} Promise com o resultado da operação - ID do produto atualizado
+ *
+ * async - updateProduct passando id e updates como parametros com validação de if.
+ *
+ * criando variavel para pegar o produto com id
+ * verificação com if se o produto existe
+ *
+ * criando a variavel para atualizar os campos fornecidos e um return dos dados atualizados - updateItem => method
+  */
