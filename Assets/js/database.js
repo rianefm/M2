82f59372970ab4718867665418f408c1216a1b0c
Jonh Alex <122692601+Jonhvmp@users.noreply.github.com>
2024-12-01 03:11:15 -0300

    Corrigir erro de digitação na função onsuccess e adicionar métodos para adicionar e obter itens do banco de dados

--- Assets/js/database.js
+++ Assets/js/database.js
@@ -40,7 +40,7 @@ export class Database {
         });
       }
     }
-    request.onsucess = (event) => {
+    request.onsuccess = (event) => {
       this.db = event.target.result;
       console.log('Banco de dados inicializado com sucesso', this.dbName);
     };
@@ -49,4 +49,41 @@ export class Database {
       console.error('Erro ao inicializar o banco de dados', event.target.error);
     };
   };
+
+  addItem(storeName, data) {
+    return new Promise((resolve, reject) => {
+      const transaction = this.db.transaction(storeName, "readwrite");
+      const store = transaction.objectStore(storeName);
+
+      const request = store.add(data);
+
+      request.onsuccess = () => {
+        console.log(`Item adicionado na tabela ${storeName}:`, data);
+        resolve(request.result); // Retorna o ID do item adicionado
+      };
+
+      request.onerror = () => {
+        console.error(`Erro ao adicionar item na tabela ${storeName}:`, request.error);
+        reject(request.error);
+      };
+    });
+  }
+
+  getAllItems(storeName) {
+    return new Promise((resolve, reject) => {
+      const transaction = this.db.transaction(storeName, "readonly");
+      const store = transaction.objectStore(storeName);
+
+      const request = store.getAll();
+
+      request.onsuccess = () => {
+        resolve(request.result); // Retorna todos os itens da tabela
+      };
+
+      request.onerror = () => {
+        console.error(`Erro ao obter itens da tabela ${storeName}:`, request.error);
+        reject(request.error);
+      };
+    });
+  }
 }
