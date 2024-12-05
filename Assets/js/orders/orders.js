c23a3b35fe14df5ae8baf3dfaad8a97130ec62d2
Riane <rianefmz@gmail.com>
2024-12-05 18:42:46 -0300

    feat(orders): implementa método para adicionar pedidos ao banco de dados

--- Assets/js/orders/orders.js
+++ Assets/js/orders/orders.js
@@ -1,30 +1,29 @@
 import { Database } from "../database";
 
 export class Orders {
-    constructor() {
-        this.db = new Database();
-        this.storeName = this.db.storeNames.orders;
+  constructor() {
+    this.db = new Database();
+    this.storeName = this.db.storeNames.orders;
+  }
+
+  async addOrder(userId, items) {
+    if (!userId || !items || !items.length === 0) {
+      throw new Error('Id e items são obrigatórios!')
     }
-
-    async addOrder(userId, items){
-      if(!userId || !items || !items.length === 0){
-        throw new Error('Id e items são obrigatórios!')
-      }
-      const order = {
-        userId,
-        items,
-        date: new Date().toISOString(),
-      };
-
-
-    }
+    const order = {
+      userId,
+      items,
+      date: new Date().toISOString(),
+    };
+    return await this.db.addItem(this.storeName, order);
+  }
 
 }
 
 
- /**
-     * Adiciona um novo pedido ao banco de dados.
-     * @param {number} userID ID do usuário que realizou o pedido
-     * @param {Array} items Array de objetos contendo { productID, quantity }
-     * @returns {Promise<number>} Retorna o ID do pedido adicionado
-     */
+/**
+    * Adiciona um novo pedido ao banco de dados.
+    * @param {number} userID ID do usuário que realizou o pedido
+    * @param {Array} items Array de objetos contendo { productID, quantity }
+    * @returns {Promise<number>} Retorna o ID do pedido adicionado
+    */
